cafec3f60d989119bcd2ddfb2af708f80fd08d93
lbb-lab <1193797659@qq.com>
2021-03-11 14:18:24 +0800

    关键字权限访问测试

--- src/test/java/life/majiang/community/test/keywordaccesspermissions/MainClass.java
+++ src/test/java/life/majiang/community/test/keywordaccesspermissions/MainClass.java
@@ -1,5 +1,7 @@
 package life.majiang.community.test.keywordaccesspermissions;
 
+import life.majiang.community.test.multithreads.ThreadTest;
+
 /**
  * @author: liu bin bin
  * @Date: 2021/3/11 10:08
@@ -14,5 +16,6 @@ public class MainClass {
         //testClass.subMoney("zhangsan",300);  //private方法，只有它内部可以访问
                                                               //protected方法，只有继承他的类可以访问，继承不能访问private
                                                               //不写权限关键字，默认为friendly，同包下才能访问
+//        testClass.setThreadTest();
     }
 }
--- src/test/java/life/majiang/community/test/keywordaccesspermissions/TestClass.java
+++ src/test/java/life/majiang/community/test/keywordaccesspermissions/TestClass.java
@@ -1,5 +1,7 @@
 package life.majiang.community.test.keywordaccesspermissions;
 
+import life.majiang.community.test.multithreads.ThreadTest;
+
 /**
  * @author: liu bin bin
  * @Date: 2021/3/11 10:00
@@ -8,6 +10,8 @@ package life.majiang.community.test.keywordaccesspermissions;
 public class TestClass {
 
     Integer money = 0;
+
+    private ThreadTest threadTest;
 
     public void saveMoney(String name,Integer money){
         this.money+=money;
@@ -19,9 +23,19 @@ public class TestClass {
         System.out.println(name+"取了："+money+"￥");
     }
 
+    public Integer getMoney() {
+        return money;
+    }
 
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
 
+    public ThreadTest getThreadTest() {
+        return threadTest;
+    }
 
-
-
+    public void setThreadTest(ThreadTest threadTest) {
+        this.threadTest = threadTest;
+    }
 }
