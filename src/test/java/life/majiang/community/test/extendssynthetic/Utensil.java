6af73102aec84768bf81dd1606c158e3e16028a0
lbb-lab <1193797659@qq.com>
2021-03-13 20:57:48 +0800

    extendssynthetic测试

---
+++ src/test/java/life/majiang/community/test/extendssynthetic/Utensil.java
@@ -0,0 +1,12 @@
+package life.majiang.community.test.extendssynthetic;
+
+/**
+ * @author: liu bin bin
+ * @Date: 2021/3/13 13:19
+ * @Description:
+ */
+public class Utensil {
+    Utensil(int i){
+        System.out.println("Utensil constructor");
+    }
+}
